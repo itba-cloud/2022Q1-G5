913e260ec952125e2a7cb0c0d40d0f2a0bedd19c
Maximiliano Lombardia <mlombardia@itba.edu.ar>
2022-06-20 23:44:23 -0300

    feat: entrega

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# repo-template
-Repositorio correspondiente al Grepo-template
+# Cloud-G5-2022
+
+
+#Step 1
+
+$terraform init
+
+#Step 2
+
+$aws configure
+
+Llenar usuario y contraseña
+Si no pide session token, llenarlo manualmente en "~/.aws/credentials"
+
+
+#Step 3
+
+$terraform plan
+
+
+#Step 4
+
+$terraform apply
+
+
+# Lambda
+
+Para ejecutar el lambda se debe enviar un json en el body con un key "title" con el cual se guardara el archivo dentro de s3 y dynamo
---
+++ resources/lambda/uploadMP3.py
@@ -0,0 +1,32 @@
+import boto3
+import json
+import os
+
+def main(event, context):
+    client = boto3.client('ssm')
+    bucket_name = os.environ.get('bucket_name')
+    #Upload file into s3
+    s3 = boto3.client('s3')
+    uploadByteStream = bytes(json.dumps(event).encode('UTF-8'))
+    s3.put_object(Body=uploadByteStream,
+     Bucket=bucket_name,
+     Key= event['title'])
+    
+    #Upload row into dynamoDB
+    dynamodb = boto3.client('dynamodb')
+    table_name = os.environ.get('dynamo_table_name')
+    url = f"https://s3.us-east-1.amazonaws.com/{bucket_name}/{event['title']}"
+    dynamodb.put_item(TableName=table_name, Item={'Title': {'S': event['title'] },'Url': {'S' : url}})
+
+    body = f"Se ha ingresado el archivo {event['title']} correctamente"
+    resp = {
+		"statusCode": 200,
+		"headers": {
+			"Access-Control-Allow-Origin": "*",
+		},
+		"body": body
+	  }
+	
+	
+
+    return resp
